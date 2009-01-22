7bc6e8508cb3f4189360edca8430ea68154c89e7
Mike Arace <mike@18thstreetsoftware.com>
2009-01-22 00:57:07 -0800

    Fixing Brett's markup :)

--- examples/StandardWidgets.php
+++ examples/StandardWidgets.php
@@ -5,26 +5,26 @@ class StandardWidgets {
 	private static function getAdditionalAttributes($args = array(), $reservedAttributes = array())
 	{
 		$attributes = '';
-                foreach ($args as $name => $value) {
-                        if (!in_array($name, $reservedAttributes)) {
-                                $attributes .= ' '.$name.'="'.htmlentities($value).'"';
-                        }
-                }
+		foreach ($args as $name => $value) {
+			if (!in_array($name, $reservedAttributes)) {
+				$attributes .= ' '.$name.'="'.htmlentities($value).'"';
+			}
+		}
 
 		return $attributes;
 	}
-	
+
 	public static function navContainer($args = array(), $innerContent = '') {
 		$attributes = self::getAdditionalAttributes($args, array('class'));
 		return '<ul class="nav'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.'>'.$innerContent.'</ul>';
 	}
-	
+
 	public static function navitemContainer($args = array(), $innerContent = '') {
 		$reservedAttributes = array('url', 'current');
 		// add the current class if set
 		if (isset($args['current']) && $args['current'] == 'true') {
 			if (isset($args['class'])) {
-				$args['class'] = 'current-nav '.$args['class']; 
+				$args['class'] = 'current-nav '.$args['class'];
 			} else {
 				$args['class'] = 'current-nav';
 			}
@@ -34,25 +34,25 @@ class StandardWidgets {
 
 		return '<li'.$attributes.'>'.(isset($args['url']) ? '<a href="'.htmlentities($args['url']).'">' : '').$innerContent.(isset($args['url']) ? '</a>' : '').'</li>';
 	}
-	
+
 	public static function subnavContainer($args = array(), $innerContent = '') {
 		$attributes = self::getAdditionalAttributes($args, array('class'));
 		return '<ul class="subnav'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.'>'.$innerContent.'</ul>';
 	}
-	
+
 	public static function bannerTag ($args = array()) {
 		$reservedAttributes = array('class', 'src', 'title', 'subtitle');
 		$attributes = self::getAdditionalAttributes($args, $reservedAttributes);
 
 		if (isset($args['src']) && file_exists($_SERVER['DOCUMENT_ROOT'].$args['src'])) {
-			$showImage = true;	
+			$showImage = true;
 		} else {
 			$showImage = false;
 		}
 
 		return '<div class="banner'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.'>'.($showImage ? '<img src="'.htmlentities($args['src']).'" alt="'.(isset($args['title']) ? htmlentities($args['title']) : 'Logo').'" />' : '').(isset($args['title']) ? '<h1>'.htmlentities($args['title']).'</h1>' : '').(isset($args['subtitle']) ? '<h2>'.htmlentities($args['subtitle']).'</h2>' : '').'</div>';
 	}
-	
+
 	public static function tabsContainer($args = array(), $innerContent = '') {
 		// set some defaults.
 		if (!$args['id']) {
@@ -74,23 +74,23 @@ class StandardWidgets {
 		$retval .= "
 			<script>
 				stdWidgetActiveTab = ".($args['current'] ? "stdWidgetTabs.indexOf('".$args['current']."')" : '0').";
-				
-			    new Ext.TabPanel({
-			        renderTo: '".$args['id']."',
-			        width: ".$args['width'].",
-			        ".($args['plain'] && $args['plain'] == 'false' ? "plain:false," : "plain:true,")."
-			        ".($args['height'] && is_numeric($args['height']) ? "height: ".$args['height']."," : '')."
-			        activeTab: stdWidgetActiveTab,
-			        ".($args['frame'] && $args['frame'] == 'true' ? "frame:true," : "frame:false,")."
-			        ".($args['hideborders'] && $args['hideborders'] == 'true' ? "hideBorders:true," : "hideBorders:false,")."
-			        ".($args['autoheight'] && $args['autoheight'] == 'true' ? 'defaults:{autoHeight: true},' : '')."
-			        items: stdWidgetTabContent
-			    });
+
+				new Ext.TabPanel({
+					renderTo: '".$args['id']."',
+					width: ".$args['width'].",
+					".($args['plain'] && $args['plain'] == 'false' ? "plain:false," : "plain:true,")."
+					".($args['height'] && is_numeric($args['height']) ? "height: ".$args['height']."," : '')."
+					activeTab: stdWidgetActiveTab,
+					".($args['frame'] && $args['frame'] == 'true' ? "frame:true," : "frame:false,")."
+					".($args['hideborders'] && $args['hideborders'] == 'true' ? "hideBorders:true," : "hideBorders:false,")."
+					".($args['autoheight'] && $args['autoheight'] == 'true' ? 'defaults:{autoHeight: true},' : '')."
+					items: stdWidgetTabContent
+				});
 			</script>
 		";
 		return $retval;
 	}
-	
+
 	public static function tabContainer($args = array(), $innerContent = '') {
 		// set some defaults.
 		if (!$args['id']) {
@@ -114,7 +114,7 @@ class StandardWidgets {
 		";
 		return $retval;
 	}
-	
+
 	public static function panelContainer($args = array(), $innerContent = '') {
 		// set some defaults.
 		if (!$args['id']) {
@@ -137,29 +137,29 @@ class StandardWidgets {
 		if (!$args['embed']) {
 			$retval = '<div id="'.$args['id'].'"'.$attributes.'></div><script>';
 			$retval .= "    new Ext.Panel({
-					        title: '".htmlentities($args['label'])."',
-					        ".($args['collapsible'] && $args['collapsible'] == 'true' ? 'collapsible:true,' : 'collapsible: false,')."
-					        ".($args['class'] ? "cls: '".str_replace("'", "\\'", $args['class'])."'," : '')."
-                                                renderTo: '".$args['id']."',
-					        width:".$args['width'].",
-					        html: html_entity_decode('".htmlentities(str_replace("\n", '', str_replace('\'', "\\'", $innerContent)))."') 
-					    })
+							title: '".htmlentities($args['label'])."',
+							".($args['collapsible'] && $args['collapsible'] == 'true' ? 'collapsible:true,' : 'collapsible: false,')."
+							".($args['class'] ? "cls: '".str_replace("'", "\\'", $args['class'])."'," : '')."
+												renderTo: '".$args['id']."',
+							width:".$args['width'].",
+							html: html_entity_decode('".htmlentities(str_replace("\n", '', str_replace('\'', "\\'", $innerContent)))."')
+						})
 					";
 			$retval .= ';</script>';
-        } else {
+		} else {
 			$retval .= "  ,{
-					        title: '".htmlentities($args['label'])."',
-					        ".($args['collapsible'] && $args['collapsible'] == 'true' ? 'collapsible:true,' : 'collapsible: false,')."
-                                                ".($args['class'] ? "cls: '".str_replace("'", "\\'", $args['class'])."'," : '')."
-                                                width:".$args['width'].",
-					        html: html_entity_decode('".htmlentities(str_replace("\n", '', str_replace('\'', "\\'", $innerContent)))."') 
-					    }
+							title: '".htmlentities($args['label'])."',
+							".($args['collapsible'] && $args['collapsible'] == 'true' ? 'collapsible:true,' : 'collapsible: false,')."
+												".($args['class'] ? "cls: '".str_replace("'", "\\'", $args['class'])."'," : '')."
+												width:".$args['width'].",
+							html: html_entity_decode('".htmlentities(str_replace("\n", '', str_replace('\'', "\\'", $innerContent)))."')
+						}
 					";
 		}
-		
+
 		return $retval;
 	}
-	
+
 	public static function dashboardContainer($args = array(), $innerContent = '') {
 		if (!$args['id']) {
 			$args['id'] = 'dashboard-'.time();
@@ -181,7 +181,7 @@ class StandardWidgets {
 				border: false,
 				items: [{
 					xtype:'portal',
-					".($args['margins'] ? "margins:'".str_replace("'", "\\'", $args['margins'])."'," : '')." 
+					".($args['margins'] ? "margins:'".str_replace("'", "\\'", $args['margins'])."'," : '')."
 					items: stdWidgetColumns
 				}]
 			});
@@ -190,7 +190,7 @@ class StandardWidgets {
 		$retval .= '</script>';
 		return $retval;
 	}
-	
+
 	public static function columnContainer($args = array(), $innerContent = '') {
 		if (!$args['width'] || !is_numeric($args['width'])) {
 			$args['width'] = '1';
@@ -203,9 +203,9 @@ class StandardWidgets {
 				];
 				stdWidgetColumnElements.shift();
 				stdWidgetColumns[stdWidgetColumns.length] = {
-                                        ".($args['id'] ? "id: '".str_replace("'", "\\'", $args['id'])."'," : '')."
+										".($args['id'] ? "id: '".str_replace("'", "\\'", $args['id'])."'," : '')."
 					".($args['class'] ? "cls: '".str_replace("'", "\\'", $args['class'])."'," : '')."
-                                        border: false,
+										border: false,
 					columnWidth: ".$args['width'].",
 					".($args['style'] ? "style: '".str_replace("'", "\\'", $args['style'])."'," : '')."
 					items: stdWidgetColumnElements
@@ -214,8 +214,8 @@ class StandardWidgets {
 			</script>
 		";
 		return $retval;
-    }
-	
+	}
+
 	public static function treeObject($args = array(), $innerXML = '') {
 		if (!$args['id']) {
 			$args['id'] = 'tree-'.time();
@@ -276,7 +276,7 @@ class StandardWidgets {
 				}
 			}
 			$itemsString .= ",cls:'".$className."'";
-			
+
 			// finalize based on node type
 			if ($child->getName() == 'leaf') {
 				$itemsString .= '}';
@@ -375,7 +375,7 @@ class StandardWidgets {
 		}
 		return $itemsString;
 	}
-	
+
 	public static function gridObject($args = array(), $innerXML = '') {
 		if (!$args['id']) {
 			$args['id'] = 'grid-'.time();
@@ -389,115 +389,172 @@ class StandardWidgets {
 		}
 		$retval = '<div id="'.$args['id'].'"'.$attributes.'></div>'."\n".'<script>'."\n";
 		$xml = new SimpleXMLElement($innerXML);
+
+		$retval .= '</script>';
+		return $retval;
 	}
-	
+
 	public static function filterContainer($args = array(), $innerContent = '') {
 		$attributes = self::getAdditionalAttributes($args, array('class'));
 
 		return '<div class="filter'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.'>'.$innerContent.'<br class="clearer" /></div>';
 	}
-	
+
 	public static function headerContainer($args = array(), $innerContent = '') {
 		$attributes = self::getAdditionalAttributes($args, array('class'));
 
 		return '<div class="header'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.'>'.$innerContent.'<br class="clearer" /></div>';
 	}
-	
+
 	public static function footerContainer($args = array(), $innerContent = '') {
 		$attributes = self::getAdditionalAttributes($args, array('class'));
 
 		return '<div class="footer'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.'>'.$innerContent.'<br class="clearer" /></div>';
 	}
-	
+
 	public static function contentContainer($args = array(), $innerContent = '') {
 		$attributes = self::getAdditionalAttributes($args, array('class'));
 
 		return '<div class="content'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.'>'.$innerContent.'<br class="clearer" /></div>';
 	}
-	
+
 	public static function layoutContainer($args = array(), $innerContent = '') {
-		
+
 	}
-	
+
 	public static function toolbarObject($args = array(), $innerXML = '') {
-		
+
 	}
-	
+
 	public static function menuObject($args = array(), $innerXML = '') {
-		
+
 	}
-	
+
 	public static function textTag ($args = array()) {
-		if (!isset($args['id'])) $args['id'] = uniqid(); 
+		if (!isset($args['id'])) $args['id'] = uniqid();
 		$attributes = self::getAdditionalAttributes($args, array('type', 'label', 'class'));
-		return '<label for="'.$args['id'].'">&nbsp;<input type="text" class="form-text'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.'/>';
+		$retval = '';
+		if (isset($args['label'])) {
+			$retval .= '<div class="form-element-container"><label for="'.$args['id'].'">'.$args['label'].'</label>';
+		}
+		$retval .= '<input type="text" class="form-text'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.' />';
+		if (isset($args['label'])) {
+			$retval .= '</div>';
+		}
+		return $retval;
 	}
-	
+
 	public static function passwordTag ($args = array()) {
-		if (!isset($args['id'])) $args['id'] = uniqid(); 
+		if (!isset($args['id'])) $args['id'] = uniqid();
 		$attributes = self::getAdditionalAttributes($args, array('type', 'label', 'class'));
-		return '<label for="'.$args['id'].'">&nbsp;<input type="password" class="form-password'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.'/>';
-		
+		$retval = '';
+		if (isset($args['label'])) {
+			$retval .= '<div class="form-element-container"><label for="'.$args['id'].'">'.$args['label'].'</label>';
+		}
+		$retval .= '<input type="password" class="form-password'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.' />';
+		if (isset($args['label'])) {
+			$retval .= '</div>';
+		}
+		return $retval;
 	}
-	
+
 	public static function dateTag ($args = array()) {
-		
+
 	}
-	
+
 	public static function selectContainer($args = array(), $innerContent = '') {
-		if (!isset($args['id'])) $args['id'] = uniqid(); 
+		if (!isset($args['id'])) $args['id'] = uniqid();
 		$attributes = self::getAdditionalAttributes($args, array('label', 'class'));
-		return '<label for="'.$args['id'].'">&nbsp;<select class="form-select'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.'>'.$innerContent.'</select>';
+		$retval = '';
+		if (isset($args['label'])) {
+			$retval .= '<div class="form-element-container"><label for="'.$args['id'].'">'.$args['label'].'</label>';
+		}
+		$retval .= '<select class="form-select'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.'>'.$innerContent.'</select>';
+		if (isset($args['label'])) {
+			$retval .= '</div>';
+		}
+		return $retval;
 	}
-	
+
 	public static function optionContainer($args = array(), $innerContent = '') {
-		if (!isset($args['id'])) $args['id'] = uniqid(); 
+		if (!isset($args['id'])) $args['id'] = uniqid();
 		$attributes = self::getAdditionalAttributes($args, array('class'));
 		return '<option class="form-option'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.'>'.$innerContent.'</option>';
 	}
 
 	public static function fileTag ($args = array()) {
-		if (!isset($args['id'])) $args['id'] = uniqid(); 
+		if (!isset($args['id'])) $args['id'] = uniqid();
 		$attributes = self::getAdditionalAttributes($args, array('type', 'label', 'class'));
-		return '<label for="'.$args['id'].'">&nbsp;<input type="file" class="form-file'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.'/>';
-		
+		$retval = '';
+		if (isset($args['label'])) {
+			$retval .= '<div class="form-element-container"><label for="'.$args['id'].'">'.$args['label'].'</label>';
+		}
+		$retval .= '<input type="file" class="form-file'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.' />';
+		if (isset($args['label'])) {
+			$retval .= '</div>';
+		}
+		return $retval;
 	}
-	
+
 	public static function checkboxTag ($args = array()) {
-		if (!isset($args['id'])) $args['id'] = uniqid(); 
+		if (!isset($args['id'])) $args['id'] = uniqid();
 		$attributes = self::getAdditionalAttributes($args, array('type', 'label', 'class'));
-		return '<input type="checkbox" class="form-checkbox'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.'/>&nbsp;<label for="'.$args['id'].'">';
-		
+		return '<input type="checkbox" class="form-checkbox'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.' /><label class="form-checkbox-label" for="'.$args['id'].'">'.$args['label'].'</label>';
+
 	}
-	
+
 	// this needs to be an object because the inner checkbox elements depend on the outer checkboxes element for their name/id
 	public static function checkboxesObject($args = array(), $innerXML = '') {
-		
+
 	}
-	
+
 	public static function radiosObject($args = array(), $innerXML = '') {
-		
+
 	}
-	
+
 	public static function textareaContainer($args = array(), $innerContent = '') {
-		if (!isset($args['id'])) $args['id'] = uniqid(); 
-		$attributes = self::getAdditionalAttributes($args, array('class'));
-		return '<textarea class="form-textarea'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.'>'.$innerContent.'</textarea>';
+		if (!isset($args['id'])) $args['id'] = uniqid();
+		$attributes = self::getAdditionalAttributes($args, array('class', 'label'));
+		$retval = '';
+		if (isset($args['label'])) {
+			$retval .= '<div class="form-element-container"><label for="'.$args['id'].'">'.$args['label'].'</label>';
+		}
+		$retval .= '<textarea class="form-textarea'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.'>'.$innerContent.'</textarea>';
+		if (isset($args['label'])) {
+			$retval .= '</div>';
+		}
+		return $retval;
 	}
-	
+
 	public static function submitTag ($args = array()) {
-		if (!isset($args['id'])) $args['id'] = uniqid(); 
-		$attributes = self::getAdditionalAttributes($args, array('type', 'class'));
-		return '<input type="submit" class="form-submit'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.'/>';
+		if (!isset($args['id'])) $args['id'] = uniqid();
+		$attributes = self::getAdditionalAttributes($args, array('type', 'class', 'label'));
+		$retval = '';
+		if (isset($args['label'])) {
+			$retval .= '<div class="form-element-container"><label for="'.$args['id'].'">'.$args['label'].'</label>';
+		}
+		$retval .= '<input type="submit" class="form-submit'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.' />';
+		if (isset($args['label'])) {
+			$retval .= '</div>';
+		}
+		return $retval;
 	}
-	
+
 	public static function buttonTag ($args = array()) {
-		if (!isset($args['id'])) $args['id'] = uniqid(); 
-		$attributes = self::getAdditionalAttributes($args, array('type', 'class'));
-		return '<input type="button" class="form-submit'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.'/>';
+		if (!isset($args['id'])) $args['id'] = uniqid();
+		$attributes = self::getAdditionalAttributes($args, array('type', 'class', 'label'));
+		$retval = '';
+		if (isset($args['label'])) {
+			$retval .= '<div class="form-element-container"><label for="'.$args['id'].'">'.$args['label'].'</label>';
+		}
+		$retval .= '<input type="button" class="form-submit'.(isset($args['class']) ? ' '.$args['class'] : '').'"'.$attributes.' />';
+		if (isset($args['label'])) {
+			$retval .= '</div>';
+		}
+		return $retval;
 	}
-	
+
 	public static function setObject($args = array(), $innerXML = '') {
-		
+
 	}
 }
--- examples/index.php
+++ examples/index.php
@@ -1,5 +1,7 @@
 <?
 
+ini_set('display_errors', 'On');
+
 require_once('../Toasty.php');
 require_once('StandardWidgets.php');
 
@@ -7,6 +9,7 @@ $toasty = new Toasty(array('widget_class' => 'StandardWidgets', 'aggregate_block
 
 $toasty->pageTitle = 'Sample Page Title';
 $toasty->tabs = array('index.php?template=example1' => 'Example 1', 'index.php?template=example2' => 'Example 2','index.php?template=example3' => 'Example 3','index.php?template=example4' => 'Example 4');
+$toasty->subnavtabs = array('index.php?template=example1' => 'Example 1', 'index.php?template=example2' => 'Example 2','index.php?template=example3' => 'Example 3','index.php?template=example4' => 'Example 4');
 
 $toasty->setRootDirectory('templates/');
 $toasty->render('index', null, true, true, true, true);
